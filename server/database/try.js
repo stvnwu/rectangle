b7a8ee3477c2ae339d75c8766e321e5a86975154
Kiri Strack-Grose <kiristrackgrose@gmail.com>
2015-08-28 20:11:19 -0700

    (doc) attempt to work with pgsql

---
+++ server/database/try.js
@@ -0,0 +1,10 @@
+var User = require('./users/user');
+var Users = require('./users/users');
+
+new User({email: 'example@email.com', password: '1234'})
+// .fetch()
+// .then(function() {
+//   Users.fetch.then(function(allUsers) {
+//     console.log(allUsers);
+//   });
+// });
