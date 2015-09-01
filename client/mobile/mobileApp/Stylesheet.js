2eb5acb67ab6cdb850f0b1042581968b4a40f1ee
Steven Wu <wucommasteven@gmail.com>
2015-08-31 17:36:12 -0700

    Closes #59

---
+++ client/mobile/mobileApp/Stylesheet.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+  StyleSheet,
+} = React;
+
+var styles = StyleSheet.create({
+  button: {
+    height: 36,
+    margin: 10,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+    buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  container: {
+    marginTop: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInput: {
+    height: 36,
+    padding: 10,
+    margin: 10,
+    flex: 1,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    flexDirection: 'row',
+    color: '#48BBEC'
+  },
+  text: {
+    color: 'black',
+    fontSize: 30,
+    margin: 80
+  }
+  
+
+});
+
+module.exports = styles;
