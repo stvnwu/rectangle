c1bf5b658aa2c51f71cc3fcfff20e09c0d975474
Steven Wu <wucommasteven@gmail.com>
2015-08-31 19:02:04 -0700

    Closes #58

--- client/mobile/mobileApp/Views/Login.js
+++ client/mobile/mobileApp/Views/Login.js
@@ -14,62 +14,26 @@ var {
 
 var styles = StyleSheet.create({
   container: {
-    marginTop: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  textInput: {
-    height: 36,
-    padding: 10,
-    margin: 10,
-    flex: 1,
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: '#48BBEC',
-    borderRadius: 8,
-    flexDirection: 'row',
-    color: '#48BBEC'
-  },
-  text: {
-    color: 'black',
-    fontSize: 30,
-    margin: 80
-  },
-  button: {
-    height: 36,
-    margin: 10,
-    backgroundColor: '#48BBEC',
-    borderColor: '#48BBEC',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 10,
-    alignSelf: 'stretch',
-    justifyContent: 'center'
-  },
-  buttonText: {
-  fontSize: 18,
-  color: 'white',
-  alignSelf: 'center'
-},
-
+    marginTop:65,
+  }
 });
 
 class Login extends Component {
   render() {
     return (
       <View style={styles.container}>
-      <Text style={styles.text}>
+      <Text style={globalStyles.text}>
         Welcome Back
       </Text>
         <TextInput
-            style={styles.textInput}
+            style={globalStyles.textInput}
             placeholder='Email...'/>
         <TextInput
-            style={styles.textInput}
+            style={globalStyles.textInput}
             placeholder='Password...'/>
-        <TouchableHighlight style={styles.button}
+        <TouchableHighlight style={globalStyles.button}
              underlayColor='#99d9f4'>
-           <Text style={styles.buttonText}>
+           <Text style={globalStyles.buttonText}>
               Send
            </Text>
          </TouchableHighlight>
--- client/mobile/mobileApp/Views/Signup.js
+++ client/mobile/mobileApp/Views/Signup.js
@@ -14,68 +14,32 @@ var {
 
 var styles = StyleSheet.create({
   container: {
-    marginTop: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  textInput: {
-    height: 36,
-    padding: 10,
-    margin: 10,
-    flex: 1,
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: '#48BBEC',
-    borderRadius: 8,
-    flexDirection: 'row',
-    color: '#48BBEC'
-  },
-  text: {
-    color: 'black',
-    fontSize: 30,
-    margin: 80
-  },
-  button: {
-    height: 36,
-    margin: 10,
-    backgroundColor: '#48BBEC',
-    borderColor: '#48BBEC',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 10,
-    alignSelf: 'stretch',
-    justifyContent: 'center'
-  },
-  buttonText: {
-  fontSize: 18,
-  color: 'white',
-  alignSelf: 'center'
-},
-
+    marginTop:65,
+  }
 });
 
 class Signup extends Component {
   render() {
     return (
       <View style={styles.container}>
-      <Text style={styles.text}>
+      <Text style={globalStyles.text}>
         Sign Up now...
       </Text>
         <TextInput
-            style={styles.textInput}
+            style={globalStyles.textInput}
             placeholder='Name...'/>
         <TextInput
-            style={styles.textInput}
+            style={globalStyles.textInput}
             placeholder='Last...'/>
         <TextInput
-            style={styles.textInput}
+            style={globalStyles.textInput}
             placeholder='Password...'/>
         <TextInput
-            style={styles.textInput}
+            style={globalStyles.textInput}
             placeholder='Email...'/>
-        <TouchableHighlight style={styles.button}
+        <TouchableHighlight style={globalStyles.button}
              underlayColor='#99d9f4'>
-           <Text style={styles.buttonText}>
+           <Text style={globalStyles.buttonText}>
               Send
            </Text>
          </TouchableHighlight>
