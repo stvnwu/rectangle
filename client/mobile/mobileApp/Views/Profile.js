5a0f910ce3b1a9bbd0b1a1e4e1969fbd70b77a3a
Andrés C. Viesca Ruiz <viestat@icloud.com>
2015-09-01 15:31:02 -0700

    Implement basic layout for Profile.js

--- client/mobile/mobileApp/Views/Profile.js
+++ client/mobile/mobileApp/Views/Profile.js
@@ -5,16 +5,28 @@ var globalStyles = require('../Stylesheet');
 var {
   AppRegistry,
   Component,
+  Image,
+  ScrollView,
   StyleSheet,
   Text,
   TextInput,
   TouchableHighlight,
-  View,
+  View, 
 } = React;
 
 var styles = StyleSheet.create({
   container: {
-    marginTop:65,
+    // marginTop:65,
+    alignItems: 'center',
+    height: 667,
+  },
+  card:{
+    width: 200,
+    height: 200,
+    alignSelf: 'center',
+  },
+  scrollView:{
+    width: 375,
   }
 });
 
@@ -22,6 +34,31 @@ class Profile extends Component {
   render() {
       return (
         <View style={styles.container}>
+        <ScrollView style={styles.scrollView}>
+          <Text style={globalStyles.prompt}>Profile</Text>
+            <Image 
+            style={styles.card}
+            source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            />
+            <TextInput
+                style={globalStyles.textInput}
+                placeholder='First name...'/>
+            <TextInput
+                style={globalStyles.textInput}
+                placeholder='Last name...'/>
+            <TextInput
+                style={globalStyles.textInput}
+                placeholder='Email...'/>
+            <TextInput
+                style={globalStyles.textInput}
+                placeholder='Phone...'/>
+            <TextInput
+                style={globalStyles.textInput}
+                placeholder='Company...'/>
+            <TextInput
+                style={globalStyles.textInput}
+                placeholder='Job title...'/>
+          </ScrollView>
         </View>
         );
   }
