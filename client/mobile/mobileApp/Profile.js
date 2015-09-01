0d69abc239dcda9d86a82ba9c018764975d0deaa
Andrés C. Viesca Ruiz <viestat@icloud.com>
2015-08-31 17:50:26 -0700

    Create template files

---
+++ client/mobile/mobileApp/Profile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var React = require('react-native');
+var globalStyles = require('./Stylesheet');
+var {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} = React;
+
+var styles = StyleSheet.create({
+  container: {
+    marginTop:65,
+  }
+});
+
+class Profile extends Component {
+  render() {
+      return (
+        <View style={styles.container}>
+        </View>
+        );
+  }
+}
+
+module.exports = Profile;
