06b8dca97f146f46e05a9d24deafb41eb996f07f
Andrés C. Viesca Ruiz <viestat@icloud.com>
2015-09-01 12:10:13 -0700

    Implement basic layout for Auth.js

--- client/mobile/mobileApp/Views/Auth.js
+++ client/mobile/mobileApp/Views/Auth.js
@@ -15,13 +15,36 @@ var {
 var styles = StyleSheet.create({
   container: {
     marginTop: 65,
-  }
+    flexDirection :'column', 
+    height: 580,
+  },
+  top: {
+    flex: 1,
+  },
+  bot: {
+    flex: 3,
+    justifyContent: 'flex-end'
+
+  },
+
 });
 
 class Auth extends Component {
   render() {
       return (
         <View style={styles.container}>
+          <View style={styles.top}>
+            <Text style={globalStyles.prompt}>WorkWhere</Text>
+          </View>
+          <View style={styles.bot}>
+          <TouchableHighlight style={globalStyles.button}>
+            <Text style={globalStyles.buttonText}>Sign Up</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={globalStyles.button}>
+            <Text style={globalStyles.buttonText}>Log In</Text>
+          </TouchableHighlight>
+
+          </View>
         </View>
         );
   }
