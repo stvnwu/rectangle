03f00dc39922782b36ec3eb82dca05eaf3ec9927
Kiri Strack-Grose <kiristrackgrose@gmail.com>
2015-09-01 14:19:42 -0700

    (refactor) add comments to guide writing tests later

--- server/test/serverTest.js
+++ server/test/serverTest.js
@@ -12,6 +12,17 @@ var userController = require('../users/userController.js');
 var cardController = require('../cards/cardController.js');
 var connectionController = require('../connections/connectionController.js');
 
+var User = require('../database/users/user');
+var Users = require('../database/users/users');
+var Card = require('../database/cards/card');
+var Cards = require('../database/cards/cards');
+var Connection = require('../database/connections/connection');
+var Connections = require('../database/connections/connections');
+
+/**
+ * Test all of the routes that the API will and will not support
+*/
+
 describe('routing test', function () {
   it("/ should return 404", function (done) {
     api.get('/')
@@ -54,29 +65,44 @@ describe('routing test', function () {
 
 });
 
+
+/**
+ * test the connection between the server and the database
+*/
 describe('connecting to the database', function () {
 
   xit('should add users to the database using POST', function (done) {
+    // use API route to write to database
+    // query database to make sure the new user is added
+    // save hashed password to a variable
 
   });
 
   xit('should edit users in the database', function (done) {
+    // use API route to edit a password
+    // query database to confirm it's not the 
+    // same hashed password as before
 
   });
 
-  xit('should remove a user\'s card(s)', function (done) {
-
-  });
-
-  xit('should remove a user form the database', function (done) {
+  xit('should remove a user from the database', function (done) {
+    // use API route to delete a user
+    // query database to confirm user doesn't exist
 
   });
 
   xit('should add a users\'s cards using post', function (done) {
+    // get a user's email
+    // use API route to add a card, tied to that email
+    // confirm that the card has been created
 
   });
 
   xit('should edit a user\'s cards', function (done) {
+
+  });
+
+  xit('should remove a user\'s card(s)', function (done) {
 
   });
 
