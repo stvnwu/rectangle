6dd26d34434d83f9f3d32d45a0b5875fadd785c3
Kiri Strack-Grose <kiristrackgrose@gmail.com>
2015-09-01 16:12:31 -0700

    (feat) initialize heroku production environments

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -8,7 +8,16 @@ module.exports = function (app, express) {
   app.use(bodyParser.urlencoded({
     extended: true
   }));
+
   app.use(bodyParser.json());
+
+  // accept CORS
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
   app.use('/users', userRouter);
   require('../users/userRoute.js')(userRouter);
 
@@ -17,11 +26,4 @@ module.exports = function (app, express) {
 
   app.use('/connections', connectionRouter);
   require('../connections/connectionRoute.js')(connectionRouter);
-
-  // accept CORS
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 };
--- server/server.js
+++ server/server.js
@@ -2,7 +2,21 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 
+
+/** @example Configuring server for Heroku with Mongo/ose
+
+ * var localuri = 'mongodb://localhost/seatly';
+ * var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || localuri;
+*/
+var host = process.env.DATABASE_URL || '127.0.0.1';
+var port = process.env.PORT || 5000;
+
 require('./config/middleware.js')(app, express);
 
-server.listen(8080);
-module.exports = app;
+server.listen(port);
+
+module.exports = {
+  'app': app,
+  'port': port,
+  'host': host
+};
