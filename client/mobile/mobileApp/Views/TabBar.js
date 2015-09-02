e6672152c0e39c76020255dac3d67d8fee54b8df
Steven Wu <wucommasteven@gmail.com>
2015-09-02 11:32:26 -0700

    init tabbar

---
+++ client/mobile/mobileApp/Views/TabBar.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var React = require('react-native');
+var globalStyles = require('../Stylesheet');
+
+var {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} = React;
+
+
+class TabBar extends Component {
+  render(){
+    
+  }
+};
+
+module.exports = TabBar;
