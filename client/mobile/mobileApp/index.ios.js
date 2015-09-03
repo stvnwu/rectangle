32280aed98fc1a5fb5543ec0a0046ffd65808ac1
Steven Wu <wucommasteven@gmail.com>
2015-09-03 12:19:35 -0700

    reinclude dumbroutes

--- client/mobile/mobileApp/index.ios.js
+++ client/mobile/mobileApp/index.ios.js
@@ -1,11 +1,28 @@
 'use strict'
 var React = require('react-native');
 var Auth = require('./Views/Auth');
+var Auth = require('./Views/Auth');
+var CardInfo = require('./Views/CardInfo');
+var Camera = require('./Views/Camera');
+var Signup = require('./Views/Signup');
+var Login = require('./Views/Login');
+var ImportCard = require('./Views/ImportCard');
+var Map = require('./Views/Map');
+var PhotoLibrary = require('./Views/PhotoLibrary');
+var Profile = require('./Views/Profile');
+var QR = require('./Views/QR');
+var QRCamera = require('./Views/QRCamera');
+var Search = require('./Views/Search');
+var TabBar = require('./Views/TabBar');
 
 var {
   AppRegistry,
+  Component,
   NavigatorIOS,
   StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
 } = React;
 
 var mobileApp = React.createClass({
@@ -28,10 +45,10 @@ var mobileApp = React.createClass({
 
     return (
       <NavigatorIOS
-        style={styles.container}
+        style={styles.navContainer}
         initialRoute={{
           title: '',
-          component: Auth,
+          component: DumbRoutes,
           passProps: {
             onExternalExampleRequested: (example) => {
               this.setState({ openExternalExample: example, });
@@ -45,14 +62,240 @@ var mobileApp = React.createClass({
   }
 });
 
+class DumbRoutes extends Component {
+  render() {
+    return(
+      <View style={styles.masterContainer}>
+        <View style={styles.top}>
+        </View>
+        <View style={styles.container}>
+          <View style={styles.flowRight}>
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'
+                  onPress={this._authHandler.bind(this)}>
+              <Text style={styles.buttonText}>Auth</Text>
+            </TouchableHighlight>
+
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'
+                  onPress={this._signUpHandler.bind(this)}>
+              <Text style={styles.buttonText}>Signup</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'
+                  onPress={this._loginHandler.bind(this)}>
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableHighlight>
+          </View>
+
+          <View style={styles.flowRight}>
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'
+                  onPress={this._importCardHandler.bind(this)}>
+              <Text style={styles.buttonText}>Import</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'
+                  onPress={this._cardInfoHandler.bind(this)}>
+              <Text style={styles.buttonText}>Card Info</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'
+                  onPress={this._photoLibraryHandler.bind(this)}>
+              <Text style={styles.buttonText}>Library</Text>
+            </TouchableHighlight>
+          </View>
+
+          <View style={styles.flowRight}>
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'
+                  onPress={this._profileHandler.bind(this)}>
+              <Text style={styles.buttonText}>Profile</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'
+                  onPress={this._mapHandler.bind(this)}>
+              <Text style={styles.buttonText}>Map</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'
+                  onPress={this._qrHandler.bind(this)}>
+              <Text style={styles.buttonText}>QR</Text>
+            </TouchableHighlight>
+          </View>
+
+          <View style={styles.flowRight}>
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'
+                  onPress={this._searchHandler.bind(this)}>
+              <Text style={styles.buttonText}>Search</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>Index</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>Blah</Text>
+            </TouchableHighlight>
+          </View>
+
+          <View style={styles.flowRight}>
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'
+                  onPress={this._tabBarHandler.bind(this)}>
+              <Text style={styles.buttonText}>TabBar</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>New</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                  underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>Newer</Text>
+            </TouchableHighlight>
+          </View>
+
+        </View>
+      </View>  
+    );
+  }
+  //here I added the event handler
+  _signUpHandler (){
+    this.props.navigator.push({
+            title: '',
+            component: Signup
+          });
+  };
+  _loginHandler (){
+    this.props.navigator.push({
+            title: '',
+            component: Login
+          });
+  };
+  _cardInfoHandler (){
+    this.props.navigator.push({
+            title: '',
+            component: CardInfo
+          });
+  };
+  _authHandler (){
+    this.props.navigator.push({
+            title: '',
+            component: Auth
+          });
+  };
+  _cameraHandler (){
+    this.props.navigator.push({
+            title: '',
+            component: Camera
+          });
+  };
+  _importCardHandler (){
+    this.props.navigator.push({
+            title: '',
+            component: ImportCard
+          });
+  };
+  _mapHandler (){
+    this.props.navigator.push({
+            title: '',
+            component: Map
+          });
+  };
+  _photoLibraryHandler (){
+    this.props.navigator.push({
+            title: '',
+            component: PhotoLibrary
+          });
+  };
+  _profileHandler (){
+    this.props.navigator.push({
+            title: '',
+            component: Profile
+          });
+  };
+  _qrHandler (){
+    this.props.navigator.push({
+            title: '',
+            component: QR
+          });
+  };
+  _qrCameraHandler (){
+    this.props.navigator.push({
+            title: '',
+            component: QRCamera
+          });
+  };
+  _searchHandler (){
+    this.props.navigator.push({
+            title: '',
+            component: Search
+          });
+  };
+
+  _tabBarHandler (){
+    this.props.navigator.push({
+            title: '',
+            component: TabBar
+    });
+  };
+
+}
+
+
+
+
 var styles = StyleSheet.create({
-  container: {
+  button: {
+    height: 36,
     flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  top: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  container: {
+    flex: 10,
+  },
+
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+
   },
   itemWrapper: {
     backgroundColor: '#eaeaea',
   },
+  masterContainer: {
+    flexDirection: 'column',
+    flex: 1,
+  },
+  navContainer: {
+    flex: 1,
+  },
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  }
 });
+
 
 AppRegistry.registerComponent('mobileApp', () => mobileApp);
 
@@ -75,19 +318,6 @@ AppRegistry.registerComponent('mobileApp', () => mobileApp);
 
 // var React = require('react-native');
 
-// var Auth = require('./Views/Auth');
-// var CardInfo = require('./Views/CardInfo');
-// var Camera = require('./Views/Camera');
-// var Signup = require('./Views/Signup');
-// var Login = require('./Views/Login');
-// var ImportCard = require('./Views/ImportCard');
-// var Map = require('./Views/Map');
-// var PhotoLibrary = require('./Views/PhotoLibrary');
-// var Profile = require('./Views/Profile');
-// var QR = require('./Views/QR');
-// var QRCamera = require('./Views/QRCamera');
-// var Search = require('./Views/Search');
-// var TabBar = require('./Views/TabBar');
 
 // var globalStyles = require('./Stylesheet');
 
@@ -290,44 +520,5 @@ AppRegistry.registerComponent('mobileApp', () => mobileApp);
 
 // }
 
-// var styles = StyleSheet.create({
-//   button: {
-//     height: 36,
-//     flex: 1,
-//     flexDirection: 'row',
-//     backgroundColor: '#48BBEC',
-//     borderColor: '#48BBEC',
-//     borderWidth: 1,
-//     borderRadius: 8,
-//     marginBottom: 10,
-//     alignSelf: 'stretch',
-//     justifyContent: 'center'
-//   },
-//   buttonText: {
-//     fontSize: 18,
-//     color: 'white',
-//     alignSelf: 'center'
-//   },
-//   container: {
-//     flex: 1,
-//     marginTop: 65,
-
-//   },
-//   flowRight: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     alignSelf: 'stretch',
-
-//   },
-//   navContainer: {
-//     flex: 1,
-//   },
-//   text: {
-//     color: 'black',
-//     backgroundColor: 'white',
-//     fontSize: 30,
-//     margin: 80
-//   }
-// });
 
 // AppRegistry.registerComponent('mobileApp', () => mobileApp);
