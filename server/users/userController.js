d74e8e6c8b2d37e355944f8bea632beb5acab243
Kiri Strack-Grose <kiristrackgrose@gmail.com>
2015-09-04 16:28:00 -0700

    (bug) add console logs to try to debug

--- server/users/userController.js
+++ server/users/userController.js
@@ -4,13 +4,13 @@ var Promise = require("bluebird");
 
 /** 
  * userRoutes is an object that contains the routes for '/users' in our API
- */
+*/
 var userRoutes = {
   /**
    * @function to sign the user in, comparing hashed passwords
    * @param {object} HTTP request object
    * @param {object} HTTP response object
-   */
+  */
   signin: function (req, res) {
     return new Promise(function (resolve, reject) {
       return new User({
@@ -19,22 +19,16 @@ var userRoutes = {
         .fetch()
         .then(function (user) {
           if (!user) {
-            //Sending a 400 response code for wrong email/password requests
             // HERE we would redirect to signup
             // confer with front end
-            res.status(400).send('email does not match');
+            res.end(JSON.stringify({
+              error: "user doesn't exist"
+            }));
           } else {
             user.comparePassword(req.body.password)
-              .then(function (isMatch) {
-                if (!isMatch) {
-                  res.status(400).send('password does not match');
-                }
-                res.end(JSON.stringify(isMatch));
-              }).catch(function (err) {
-                console.log(err);
-                res.status(400).send('password/email does not match');
-
-              });
+            .then(function(isMatch) {
+              res.end(JSON.stringify(isMatch));
+            });
           }
         })
     })
@@ -47,7 +41,7 @@ var userRoutes = {
    * @function to sign the user up and hash the password
    * @param {object} HTTP request object
    * @param {object} HTTP response object
-   */
+  */
   signup: function (req, res) {
     return new Promise(function (resolve, reject) {
       Users.query({
@@ -56,10 +50,10 @@ var userRoutes = {
         }
       }).fetchOne().then(function (user) {
         if (user) {
-          //Sending 422 response code if email already exists in DB
-          res.status(422).send('email already exists');
-        } else if (req.body.email !== "" && req.body.email.indexOf("@") !== -1 && req.body.email !== undefined) {
-          console.log(62, req.body.email);
+          res.end(JSON.stringify({
+            error: "Email already exists"
+          }));
+        } else if (req.body.email) {
           console.log(37, "email provided");
           return new User({
             email: req.body.email,
@@ -68,6 +62,7 @@ var userRoutes = {
             lastName: req.body.lastName
           }).save().then(function (newUser) {
             console.log(20, newUser);
+            res.end(newUser.get('email'));g
             res.end(JSON.stringify(newUser));
           }).catch(function (err) {
             console.log(new Error(err));
@@ -75,7 +70,9 @@ var userRoutes = {
           })
         } else {
           console.log(51, req.body);
-          res.status(400).send('email is not provided/invalid');
+          res.end(JSON.stringify({
+            error: 'enter your email'
+          }));
         }
       })
     });
