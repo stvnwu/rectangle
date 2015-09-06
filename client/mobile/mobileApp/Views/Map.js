d61dc6000baae377ee42cf422e30b158eaca163d
Steven Wu <wucommasteven@gmail.com>
2015-09-05 17:47:23 -0700

    (feat) include mapbox

--- client/mobile/mobileApp/Views/Map.js
+++ client/mobile/mobileApp/Views/Map.js
@@ -1,57 +1,134 @@
 'use strict';
 
 var React = require('react-native');
-var globalStyles = require('../Stylesheet');
+var MapboxGLMap = require('react-native-mapbox-gl');
+var mapRef = require('../apiKeys').map2;
 var {
   AppRegistry,
-  Component,
-  MapView,
   StyleSheet,
   Text,
-  TextInput,
-  TouchableHighlight,
+  StatusBarIOS,
   View,
 } = React;
 
+var Example = React.createClass({
+  mixins: [MapboxGLMap.Mixin],
+  getInitialState() {
+    return {
+       center: {
+         latitude: 40.72052634,
+         longitude: -73.97686958312988
+       },
+       zoom: 11,
+       annotations: [{
+         latitude: 40.72052634,
+         longitude:  -73.97686958312988,
+         title: 'This is marker 1',
+         subtitle: 'It has a rightCalloutAccessory too',
+         rightCalloutAccessory: {
+             url: 'https://cldup.com/9Lp0EaBw5s.png',
+             height: 25,
+             width: 25
+         },
+         annotationImage: {
+           url: 'https://cldup.com/CnRLZem9k9.png',
+           height: 25,
+           width: 25
+         },
+         id: 'marker1'
+       },{
+         latitude: 40.714541341726175,
+         longitude:  -74.00579452514648,
+         title: 'Important!',
+         subtitle: 'Neat, this is a custom annotation image',
+         annotationImage: {
+           url: 'https://cldup.com/7NLZklp8zS.png',
+           height: 25,
+           width: 25
+         },
+         id: 'marker2'
+       }]
+     };
+  },
+  onRegionChange(location) {
+    this.setState({ currentZoom: location.zoom });
+  },
+  onRegionWillChange(location) {
+    console.log(location);
+  },
+  onUpdateUserLocation(location) {
+    console.log(location);
+  },
+  onOpenAnnotation(annotation) {
+    console.log(annotation);
+  },
+  onRightAnnotationTapped(e) {
+    console.log(e);
+  },
+  render: function() {
+    StatusBarIOS.setHidden(true);
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text} onPress={() => this.setDirectionAnimated(mapRef, 0)}>
+          Set direction to 0
+        </Text>
+        <Text style={styles.text} onPress={() => this.setZoomLevelAnimated(mapRef, 6)}>
+          Zoom out to zoom level 6
+        </Text>
+        <Text style={styles.text} onPress={() => this.setCenterCoordinateAnimated(mapRef, 48.8589, 2.3447)}>
+          Go to Paris at current zoom level {parseInt(this.state.currentZoom)}
+        </Text>
+        <Text style={styles.text} onPress={() => this.setCenterCoordinateZoomLevelAnimated(mapRef, 35.68829, 139.77492, 14)}>
+          Go to Tokyo at fixed zoom level 14
+        </Text>
+        <Text style={styles.text} onPress={() => this.addAnnotations(mapRef, [{
+          latitude: 40.73312,
+          longitude:  -73.989,
+          title: 'This is a new marker'
+          }])}>
+          Add new marker
+        </Text>
+        <Text style={styles.text} onPress={() => this.selectAnnotationAnimated(mapRef, 0)}>
+          Open first popup
+        </Text>
+        <Text style={styles.text} onPress={() => this.removeAnnotation(mapRef, 0)}>
+          Remove first annotation
+        </Text>
+        <MapboxGLMap
+          style={styles.map}
+          direction={0}
+          rotateEnabled={true}
+          scrollEnabled={true}
+          zoomEnabled={true}
+          showsUserLocation={true}
+          ref={mapRef}
+          accessToken={mapRef}
+          styleURL={'asset://styles/mapbox-streets-v7.json'}
+          centerCoordinate={this.state.center}
+          userLocationVisible={true}
+          zoomLevel={this.state.zoom}
+          onRegionChange={this.onRegionChange}
+          onRegionWillChange={this.onRegionWillChange}
+          annotations={this.state.annotations}
+          onOpenAnnotation={this.onOpenAnnotation}
+          onRightAnnotationTapped={this.onRightAnnotationTapped}
+          onUpdateUserLocation={this.onUpdateUserLocation} />
+      </View>
+    );
+  }
+});
+
 var styles = StyleSheet.create({
   container: {
-    marginTop:65,
+    flexDirection: 'column',
+    flex: 1
   },
-   card:{
-    width: 300,
-    height: 300,
-    alignSelf: 'center',
+  map: {
+    flex: 5
   },
+  text: {
+    padding: 2
+  }
 });
-var region = {
-  latitude: 51.50,
-  longitude: -0.13, 
-  latitudeDelta: 0.0001, 
-  longitudeDelta: 0.0001
-}
 
-class Map extends Component {
-  _getAnnotations() {
-    return [{
-      latitude: 51.50, 
-      longitude: -0.13,
-      title: 'You Are Here',
-      animateDrop: true,
-    }];
-  }
-
-  render() {
-      return (
-        <View style={styles.container}>
-        <Text style={globalStyles.prompt}>Map</Text>
-        <MapView 
-        region={region}
-        annotations={this._getAnnotations()} 
-         style={styles.card}>
-          </MapView>
-        </View>
-        );
-  }
-}
-
-module.exports = Map;
+module.exports = Example;
