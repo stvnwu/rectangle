723cb0d32440ca5572a96f8b9f9b534a26ecc1b7
Surya kotamraju <srtejk@gmail.com>
2015-09-05 17:20:41 -0700

    update card routes to appropriate http responses

--- server/cards/cardController.js
+++ server/cards/cardController.js
@@ -7,61 +7,69 @@ var Promise = require("bluebird");
 var cardRoutes = {
   createCard: function (req, res) {
     return new Promise(function (resolve, reject) {
-      console.log(7, req.body);
-      Cards.query({
+      Users.query({
         where: {
           email: req.body.email
         }
-      }).fetchOne().then(function (card) {
-        if (card) {
-          card.save({
-            firstName: req.body.firstName || card.get("firstName"),
-            lastName: req.body.lastName || card.get("lastName"),
-            company: req.body.company || card.get("company"),
-            phone: req.body.phone || card.get("phone"),
-            jobTitle: req.body.jobTitle || card.get("jobTitle")
-          }).then(function (updatedCard) {
-            res.end(JSON.stringify(updatedCard));
-          })
+      }).fetchOne().then(function (user) {
+        if (user) {
+          Cards.query({
+            where: {
+              email: req.body.cardEmail
+            }
+          }).fetchOne().then(function (card) {
+            if (card) {
+              console.log(65, req.body.cardEmail);
+              card.save({
+                firstName: req.body.firstName || card.get("firstName"),
+                lastName: req.body.lastName || card.get("lastName"),
+                company: req.body.company || card.get("company"),
+                phone: req.body.phone || card.get("phone"),
+                jobTitle: req.body.jobTitle || card.get("jobTitle")
+              }).then(function (updatedCard) {
+                res.end(JSON.stringify(updatedCard));
+              })
+            } else {
+              return new Card({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                company: req.body.company,
+                phone: req.body.phone,
+                jobTitle: req.body.jobTitle,
+                userID: user.get("id"),
+                email: req.body.cardEmail,
+              }).save().then(function (newCard) {
+                console.log(85, newCard);
+                res.end(JSON.stringify(newCard));
+              }).catch(function (err) {
+                console.log(88, new Error(err));
+                res.end(JSON.stringify(err));
+              });
+            }
+          });
         } else {
-          //check to pass in entire object
-          return new User({
-            email: req.body.userEmail
-          }).fetch().then(function (user) {
-            return new Card({
-              firstName: req.body.firstName,
-              lastName: req.body.lastName,
-              company: req.body.company,
-              phone: req.body.phone,
-              jobTitle: req.body.jobTitle,
-              email: req.body.email,
-              userID: user.get("id")
-            }).save().then(function (newCard) {
-              console.log(20, newCard);
-              res.end(JSON.stringify(newCard));
-            }).catch(function (err) {
-              console.log(new Error(err));
-              res.end(JSON.stringify(err));
-            })
-          })
+          res.status(400).send('user not logged in');
         }
       })
     });
   },
-  getCards: function (req, res) {
-    var p = new Promise(function (resolve, reject) {
-      return Cards.query({
-          where: {
-            email: req.body.email
-          }
-        }).fetchOne()
-        .then(function (card) {
+  getCard: function (req, res) {
+    return new Promise(function (resolve, reject) {
+      Cards.query({
+        where: {
+          email: req.body.cardEmail
+        }
+      }).fetchOne().then(function (card) {
+        console.log(63, card);
+        if (card) {
           res.end(JSON.stringify(card))
-            .catch(function (err) {
-              console.log(new Error(err));
-              res.end(JSON.stringify(err));
-            })
-        });
+        } else {
+          res.status(400).send('no card found related to email');
+        }
+      }).catch(function (err) {
+        console.log(new Error(err));
+        res.end(JSON.stringify(err));
+      });
     });
   }
 }
