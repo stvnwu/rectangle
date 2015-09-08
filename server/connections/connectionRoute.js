691ed459d18eef95477caefe231d3b15e8b7a9ed
Surya kotamraju <srtejk@gmail.com>
2015-09-07 21:13:54 -0700

    add route to get connections

--- server/connections/connectionRoute.js
+++ server/connections/connectionRoute.js
@@ -3,4 +3,5 @@ var connectionController = require('./connectionController.js');
 module.exports = function (app) {
   app.post('/createconnection', connectionController.createConnection);
   app.delete('/deleteconnection', connectionController.deleteConnection);
+  app.post('/getconnections', connectionController.getConnections);
 };
