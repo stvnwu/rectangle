0e5e1b844f3a4b9b4c7962eba0b5e809158cf315
Steven Wu <wucommasteven@gmail.com>
2015-09-08 12:19:47 -0700

    (feat) create tabBar, but map icon needs a fix

--- client/mobile/mobileApp/Views/Default.js
+++ client/mobile/mobileApp/Views/Default.js
@@ -53,10 +53,10 @@ var Default = React.createClass({
 
         <TabBarItemIOS
           name="home"
-          iconName={'ion|ios-home-outline'}
-          title={''}
+          iconName={'fontawesome|home'}
+          title={'Home'}
           badgeValue={3}
-          iconSize={32}
+          iconSize={18}
           accessibilityLabel="Home Tab"
           selected={this.state.selectedTab === 'home'}
           onPress={() => {
@@ -64,15 +64,15 @@ var Default = React.createClass({
               selectedTab: 'home',
             });
           }}>
-          {this._renderContent()}
+          <Search/>
         </TabBarItemIOS>
         
 
         <TabBarItemIOS
           name="articles"
-          iconName={'ion|ios-paper-outline'}
-          title={''}
-          iconSize={32}
+          iconName={'fontawesome|qrcode'}
+          title={'QR'}
+          iconSize={18}
           accessibilityLabel="Articles Tab"
           selected={this.state.selectedTab === 'articles'}
           onPress={() => {
@@ -80,15 +80,15 @@ var Default = React.createClass({
               selectedTab: 'articles',
             });
           }}>
-          {this._renderContent()}
+          <QR/>
         </TabBarItemIOS>
         
 
         <TabBarItemIOS
           name="messages"
-          iconName={'ion|chatboxes'}
-          title={''}
-          iconSize={32}
+          iconName={'fontawesome|camera'}
+          title={'Scan'}
+          iconSize={18}
           accessibilityLabel="Messages Tab"
           selected={this.state.selectedTab === 'messages'}
           onPress={() => {
@@ -96,15 +96,15 @@ var Default = React.createClass({
               selectedTab: 'messages',
             });
           }}>
-          {this._renderContent()}
+          <Camera/>
         </TabBarItemIOS>
         
 
         <TabBarItemIOS
           name="settings"
-          iconName={'ion|ios-gear'}
-          title={''}
-          iconSize={32}
+          iconName={'fontawesome|globe'}
+          title={'Map'}
+          iconSize={18}
           accessibilityLabel="Settings Tab"
           selected={this.state.selectedTab === 'settings'}
           onPress={() => {
@@ -112,108 +112,28 @@ var Default = React.createClass({
               selectedTab: 'settings',
             });
           }}>
-          {this._renderContent()}
+          <Map/>
         </TabBarItemIOS>
 
+
+        <TabBarItemIOS
+          name="settings"
+          iconName={'fontawesome|user'}
+          title={'Profile'}
+          iconSize={18}
+          accessibilityLabel="Settings Tab"
+          selected={this.state.selectedTab === 'settings'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'settings',
+            });
+          }}>
+          <Profile/>
+        </TabBarItemIOS>
         
       </TabBarIOS>
     );
   },
-  _renderContent: function () {
-    return (
-      <ScrollView contentContainerStyle={styles.container}>
-        <View style={styles.topContainer}>
-          <TouchableHighlight
-            underlayColor="transparent"
-            onPress={() => alert('Beer!')}>
-            <Icon
-              name='ion|beer'
-              size={40}
-              color='#887700'
-              style={styles.beer}
-              />
-          </TouchableHighlight>
-          <Icon
-            name='material|face'
-            size={40}
-            color='black'
-            style={styles.github}
-            />
-          <Icon
-            name='fontawesome|facebook-square'
-            size={40}
-            color={BrandColors.Facebook}
-            style={styles.facebook}
-            />
-          <Icon
-            name='foundation|lightbulb'
-            size={40}
-            style={styles.lightbulb}/>
-        </View>
-
-        <Text style={styles.header}>
-          {'Material Icons'}
-        </Text>
-
-        <View style={styles.topContainer}>
-
-          <Spinner name='fontawesome|spinner' size={24} style={{width: 24, height: 24, backgroundColor: 'transparent'}}
-                   color='#777'/>
-          <Spinner name='ion|load-a' size={24} style={{width: 24, height: 24, backgroundColor: 'transparent'}}
-                   color='#777'/>
-          <Spinner name='ion|load-b' size={24} style={{width: 24, height: 24, backgroundColor: 'transparent'}}
-                   color='#777'/>
-          <Spinner name='ion|load-c' size={24} style={{width: 24, height: 24, backgroundColor: 'transparent'}}
-                   color='#777'/>
-          <Spinner name='ion|load-d' size={24} style={{width: 24, height: 24, backgroundColor: 'transparent'}}
-                   color='#777'/>
-        </View>
-
-        <Text style={styles.header}>
-          {'Stacked Icons!'}
-        </Text>
-        <Icon
-          name='fontawesome|square'
-          size={70}
-          color={BrandColors.Twitter}
-          style={styles.twitterOutline}>
-          <Icon
-            name='fontawesome|twitter'
-            size={40}
-            color='#ffffff'
-            style={[styles.twitterIcon, {backgroundColor: 'transparent'}]}/>
-        </Icon>
-
-        <Text style={styles.header}>
-          {'Create social sign in buttons'}
-        </Text>
-        <View
-          style={styles.signInWithTwitterButton}>
-          <Icon
-            name='fontawesome|twitter'
-            size={28}
-            color='#ffffff'
-            style={styles.signInWithTwitterIcon}/>
-          <Text style={styles.signInText}>
-            {'Sign in with Twitter'}
-          </Text>
-        </View>
-
-        <View
-          style={styles.signInWithFacebookButton}>
-          <Icon
-            name='fontawesome|facebook'
-            size={28}
-            color='#ffffff'
-            style={styles.signInWithFacebookIcon}/>
-          <Text style={styles.signInText}>
-            {'Sign in with Facebook'}
-          </Text>
-        </View>
-
-      </ScrollView>
-    );
-  }
 });
 
 
@@ -224,90 +144,8 @@ var styles = StyleSheet.create({
     color: '#ff0000',
     tintColor: '#877324'
   },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  topContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  },
-  header: {
-    flexDirection: 'column',
-    backgroundColor: 'transparent',
-    fontSize: 18,
-    marginTop: 20,
-    marginBottom: 10,
-    color: '#555555',
-  },
-  beer: {
-    width: 70,
-    height: 70,
-    margin: 10
-  },
-  github: {
-    width: 70,
-    height: 70,
-    margin: 10
-  },
-  facebook: {
-    width: 70,
-    height: 70,
-    margin: 10
-  },
-  lightbulb: {
-    width: 70,
-    height: 70,
-    margin: 10
-  },
-  twitterOutline: {
-    flexDirection: 'column',
-    width: 70,
-    height: 70,
-    alignItems: 'center'
-  },
-  twitterIcon: {
-    flex: 1,
-    width: 40,
-    height: 40
-  },
-  signInWithTwitterButton: {
-    backgroundColor: BrandColors.Twitter,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    width: 210,
-    padding: 5,
-    borderRadius: 3
-  },
-  signInWithTwitterIcon: {
-    width: 28,
-    height: 28,
-    marginLeft: 5
-  },
-  signInText: {
-    color: 'white',
-    marginLeft: 5,
-    fontFamily: 'HelveticaNeue-Medium',
-    fontSize: 15
-  },
-  signInWithFacebookButton: {
-    backgroundColor: BrandColors.Facebook,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    width: 210,
-    padding: 5,
-    borderRadius: 3,
-    marginTop: 10
-  },
-  signInWithFacebookIcon: {
-    width: 28,
-    height: 28,
-    marginLeft: 5
-  }
+  
+ 
 });
 
 module.exports = Default;
