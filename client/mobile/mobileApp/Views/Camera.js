f0695c59a8446f4ff2b01153414023b691bb8609
Andrés C. Viesca Ruiz <viestat@icloud.com>
2015-09-09 17:21:52 -0700

    Display response message on the screen

--- client/mobile/mobileApp/Views/Camera.js
+++ client/mobile/mobileApp/Views/Camera.js
@@ -2,36 +2,85 @@ var React = require('react-native');
 var Camera = require('react-native-camera');
 
 var {
+  AsyncStorage,
   StyleSheet,
   Text,
   View,
+  VibrationIOS,
   TouchableHighlight
 } = React;
+
+var reqBody = {'email': '', 'cardEmail': ''};
+
+var obj = {  
+  method: 'POST',
+  headers: {
+     'Content-Type': 'application/json',
+   },
+  body: JSON.stringify({'userEmail': null, 'cardEmail': null})
+}
 
 var CameraPage = React.createClass({
   getInitialState: function() {
     return {
-      cameraType: Camera.constants.Type.back
+      cameraType: Camera.constants.Type.back,
+      promt: 'Scan the QR to connect!',
+      readQr: false
     }
   },
 
   render: function() {
 
     return (
-      <Text style={styles.welcome}>
-        Scan the QR to connect!
-      </Text>
       <Camera
         ref="cam"
         style={styles.container}
         onBarCodeRead={(event)=> this._onBarCodeRead(event)}
         type={this.state.cameraType}
       >
+      <View style={styles.containerView}>
+        <Text style={styles.welcome}>
+          {this.state.promt}
+        </Text>
+      </View>
       </Camera>
     );
   },
-  _onBarCodeRead: function(data) {
-    console.log(data,'<----------------CAM DATA');
+  _responseHandler: function(response){
+    this.state.readQr = true;
+    var message = 'Correcto Mondo!';
+    if(response.error){
+      message = response.error;
+    } else {
+      //shold redirect to cards page
+      message = response.message;
+    }
+    this.setState((state) => {
+      return {
+        promt: message
+      };
+    });
+  },
+  _onBarCodeRead: function(scan) {
+    // console.log(scan,'<-----------SCAN')
+    if(!this.state.readQr){ 
+      VibrationIOS.vibrate();
+      AsyncStorage.getItem('userEmail')
+      .then((userEmail)=>{
+        reqBody.email = userEmail;
+        reqBody.cardEmail = JSON.parse(scan.data).cardEmail;
+
+        obj.body = JSON.stringify(reqBody);
+        console.log(obj,'<-----------REQBODY')
+        fetch('https://tranquil-earth-7083.herokuapp.com/connections/createconnection', obj)  
+          .then((res) => res.json())
+          .then((resJson) => this._responseHandler(resJson))      
+      })
+      .catch((err) => {
+        console.log(new Error(err));
+      });
+    }
+    
     
   }
 });
@@ -44,6 +93,13 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'transparent',
   },
+  containerView: {
+
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
+  },
+
   welcome: {
     color: '#333333',
     fontSize: 20,
