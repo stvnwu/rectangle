28d90fae754f92a5e1d44ec5b56e59339fd5d3b3
Kiri Strack-Grose <kiristrackgrose@gmail.com>
2015-09-16 19:35:30 -0700

    (feat) display pins with names (refactor) to ES6 (clean) and add comments

--- client/mobile/mobileApp/Views/Map.js
+++ client/mobile/mobileApp/Views/Map.js
@@ -5,6 +5,7 @@ var React = require('react-native');
 var {
   AppRegistry,
   AsyncStorage,
+  Component,
   MapView,
   ScrollView,
   StatusBarIOS,
@@ -14,17 +15,10 @@ var {
   View,
 } = React;
 
-var regionText = {
-  latitude: '0',
-  longitude: '0',
-  latitudeDelta: '0',
-  longitudeDelta: '0',
-};
 
-var mockData = [
-  {}
-];
-
+/**
+ * closure scope variables
+*/
 var obj = {  
   method: 'POST',
   headers: {
@@ -33,154 +27,50 @@ var obj = {
   body: null
 };
 
-var MapRegionInput = React.createClass({
-  propTypes: {
-    region: React.PropTypes.shape({
-      latitude: React.PropTypes.number.isRequired,
-      longitude: React.PropTypes.number.isRequired,
-      latitudeDelta: React.PropTypes.number.isRequired,
-      longitudeDelta: React.PropTypes.number.isRequired,
-    }),
-    onChange: React.PropTypes.func.isRequired,
-  },
-  getInitialState: function() {
-    return {
-      region: {
-        latitude: 0,
-        longitude: 0,
-        latitudeDelta: 0,
-        longitudeDelta: 0,
-      }
-    };
-  },
-
-  //   navigator.geolocation.getCurrentPosition(res => {
-  //   reqBody.longitude = JSON.stringify(res.coords.longitude);
-  //   reqBody.latitude = JSON.stringify(res.coords.latitude);
-  // });
-  componentWillReceiveProps: function(nextProps) {
-    this.setState({
-      region: nextProps.region || this.getInitialState().region
-    });
-  },
-
-  render: function() {
-    var region = this.state.region || this.state.region;
-
-    return (
-      <View>
-        <View style={styles.row}>
-          <Text>
-            {'Latitude'}
-          </Text>
-          <TextInput
-            value={'' + region.latitude}
-            style={styles.textInput}
-            onChange={this._onChangeLatitude}
-            selectTextOnFocus={true}
-          />
-        </View>
-        <View style={styles.row}>
-          <Text>
-            {'Longitude'}
-          </Text>
-          <TextInput
-            value={'' + region.longitude}
-            style={styles.textInput}
-            onChange={this._onChangeLongitude}
-            selectTextOnFocus={true}
-          />
-        </View>
-        <View style={styles.row}>
-          <Text>
-            {'Latitude delta'}
-          </Text>
-          <TextInput
-            value={'' + region.latitudeDelta}
-            style={styles.textInput}
-            onChange={this._onChangeLatitudeDelta}
-            selectTextOnFocus={true}
-          />
-        </View>
-        <View style={styles.row}>
-          <Text>
-            {'Longitude delta'}
-          </Text>
-          <TextInput
-            value={'' + region.longitudeDelta}
-            style={styles.textInput}
-            onChange={this._onChangeLongitudeDelta}
-            selectTextOnFocus={true}
-          />
-        </View>
-        <View style={styles.changeButton}>
-          <Text onPress={this._change}>
-            {'Change'}
-          </Text>
-        </View>
-      </View>
-    );
-  },
-
-  _onChangeLatitude: function(e) {
-    regionText.latitude = e.nativeEvent.text;
-  },
-
-  _onChangeLongitude: function(e) {
-    regionText.longitude = e.nativeEvent.text;
-  },
-
-  _onChangeLatitudeDelta: function(e) {
-    regionText.latitudeDelta = e.nativeEvent.text;
-  },
-
-  _onChangeLongitudeDelta: function(e) {
-    regionText.longitudeDelta = e.nativeEvent.text;
-  },
-
-  _change: function() {
-    this.setState({
-      latitude: parseFloat(regionText.latitude),
-      longitude: parseFloat(regionText.longitude),
-      latitudeDelta: parseFloat(regionText.latitudeDelta),
-      longitudeDelta: parseFloat(regionText.longitudeDelta),
-    });
-    this.props.onChange(this.state.region);
-  },
-
-});
-
-var MapViewExample = React.createClass({
-
-  getInitialState: function() {
-    return {
+class MapViewExample extends Component{
+  /**
+   * @method to be run upon initialization
+   * creates a state object with:
+   * mapRegion, mapRegionInput, annotations,
+   * isFirstLoad, cards, and connections
+  */
+  constructor(props) {
+    super(props);
+    this.state = {
       mapRegion: null,
       mapRegionInput: null,
       annotations: null,
       isFirstLoad: true,
       cards: null,
       connections: null
-    };
-  },
-
-  componentDidMount: function() {
+    }
+  }
+  /**
+   * @method to be run when the compontent mounts
+   * calls _getCardInfo, _getConnections, and _getAnnotations
+   * returns nothing
+  */
+  componentDidMount() {
     this._getCardInfo.bind(this)();
     this._getConnections.bind(this)();
     this._getAnnotations.bind(this)();
-  },
-
-  _getCardInfo: function() {
+  }
+  /**
+   * @method to get the card information from AsyncStorage
+  */
+  _getCardInfo() {
     AsyncStorage.getItem('cards')
     .then((cards) => {
       this.setState({
         cards: JSON.parse(cards)
       });
-      console.log('this is the state in Map.js,  line 178', this.state);
     })
     .done();
-  },
-
-  _getConnections: function() {
+  }
+  /**
+   * @method to get the connection info from the API
+  */
+  _getConnections() {
     AsyncStorage.getItem('userEmail')
     .then((userEmail) => {
       obj.body = JSON.stringify({'email': userEmail});
@@ -193,99 +83,78 @@ var MapViewExample = React.createClass({
       this.setState({
         connections: JSON.parse(response._bodyText).message
       });
-      console.log('this is the state, Map.js, line 196', this.state);
       this._getAnnotations.bind(this)();
     })
     .done();
-  },
-
-  _getAnnotations: function(region) {
+  }
+  /**
+   * @method to create annotations from the card and
+   * connection information combined
+  */
+  _getAnnotations() {
     if (this.state.cards && this.state.connections) {
       var annotations = [];
       for (var i = 0; i < this.state.connections.length; i++) {
-        var currConnection = this.state.connections[i]
+        var currConnection = this.state.connections[i]        
         var name = this._getName(currConnection.card_id);
-        var anno = {
-          longitude: currConnection.longitude,
-          latitude: currConnection.latitude,
+
+        var annotation = {
+          longitude: parseFloat(currConnection.longitude),
+          latitude: parseFloat(currConnection.latitude),
           title: name
         }
-        annotations.push(anno);
-        console.log('pushed another annotation!', annotations);
+
+        annotations.push(annotation);
       }
       this.setState({
         'annotations': annotations
       });
-      console.log('this is the state, Map.js, line 218', this.state);
+      console.log('The pins have been retrieved', 'Map.js', 98);
+      this.refs.mapRef.forceUpdate();
     }
-  },
-    // }
-    // return [{
-    //   longitude: region.longitude+5,
-    //   latitude: region.latitude+5,
-    //   title: 'You Are Here',
-    // },{
-    //   longitude: -122.4082479999,
-    //   latitude: 37.78383,
-    //   title: 'You Are Here',
-    // }];
-
-  _getName: function(cardID) {
+  }
+  /**
+   * @method (helper) to get the name from the card info
+  */
+  _getName(cardID) {
     for (var i = 0; i < this.state.cards.length; i++) {
       if (this.state.cards[i].id === cardID) {
         return this.state.cards[i].firstName + ' ' + this.state.cards[i].lastName;
       }
     }
-  },
+  }
+  /**
+   * @method render the map
+  */
+  render() {
+    var spacer = <View style={styles.spacer}/>;
+    
+    if (this.state.annotations) {
+      return (
+        <ScrollView style={styles.wrapper}>
+          <View>
+            <MapView
+              ref={'mapRef'}
+              style={styles.map}
+              annotations={this.state.annotations || undefined}
+              showsUserLocation={true}
+            />
 
-  _onRegionChange: function(region) {
-    this.setState({
-      mapRegionInput: region,
-    });
-  },
-
-  _onRegionChangeComplete: function(region) {
-    if (this.state.isFirstLoad) {
-      this.setState({
-        mapRegionInput: region,
-        annotations: this._getAnnotations(region),
-        isFirstLoad: false,
-      });
+          </View>
+          {spacer}
+        </ScrollView>
+      );
+    } else {
+      return (
+        <ScrollView style={styles.wrapper}>
+          <Text>
+            LOADING TEXTS
+          </Text>
+        </ScrollView>
+      )
     }
-  },
-
-  _onRegionInputChanged: function(region) {
-    this.setState({
-      mapRegion: region,
-      mapRegionInput: region,
-      annotations: this._getAnnotations(region),
-    });
-  },
-
-  render: function() {
-    var spacer=<View style={styles.spacer}/>;
-    return (
-      <ScrollView styles={styles.wrapper}>
-        <View>
-          <MapView
-            style={styles.map}
-            onRegionChange={this._onRegionChange}
-            onRegionChangeComplete={this._onRegionChangeComplete}
-            region={this.state.mapRegion || undefined}
-            annotations={this.state.annotations || this.state.annotations}
-            showsUserLocation={true}
-          />
-          <MapRegionInput
-            onChange={this._onRegionInputChanged}
-            region={this.state.mapRegionInput || undefined}
-          />
-        </View>
-        {spacer}
-      </ScrollView>
-    );
-  },
-
-});
+  }
+};
 
 var styles = StyleSheet.create({
   changeButton: {
